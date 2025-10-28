d9263103ae12b5f4a25e93104aa13ce49db95795
mrmarkuz <mrmarkuz666@gmail.com>
2025-10-28 15:26:16 +0100

    Fix container id error

--- build-images.sh
+++ build-images.sh
@@ -41,7 +41,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:15.8-alpine3.19 docker.io/redis:8.2.2 docker.io/calcom/cal.com:v5.8.2"
+    --label="org.nethserver.images=docker.io/postgres:15.8-alpine3.19 docker.io/redis:8.2.2 docker.io/calcom/cal.com:v5.8.2" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
